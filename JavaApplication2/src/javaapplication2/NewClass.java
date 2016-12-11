935cd9140c25bb399eaaa6ea5466a75f1582311f
love <love@love-workplace>
2016-12-11 16:32:17 +0800

    aaa

---
+++ JavaApplication2/src/javaapplication2/NewClass.java
@@ -0,0 +1,15 @@
+package test.JavaApplication2.src.javaapplication2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author love
+ */
+public class NewClass {
+    
+}
